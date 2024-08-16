ecb0287bb79a095f06e1d6916c3f543c8b7cfcc8
Simrath Randhawa <srandhawa1250@gmail.com>
2024-08-16 14:40:32 -0500

    functionality for click area see how many users is working

--- scripts/areas.js
+++ scripts/areas.js
@@ -1,12 +1,47 @@
-import { getAreas} from "./database.js"
+import { getAreas, getGuests} from "./database.js"
 
-const guests = getAreas()
+const areas = getAreas()
+const guests = getGuests()
 
 export const Areas = () => {
-    return `
-        <ul>
-            <li>Area 1</li>
-            <li>Area 2</li>
-        </ul>
-    `;
+
+    let areaHtml = '<ol>'
+
+    for (const area of areas){
+        areaHtml += `<li data-type="area" data-region="${area.region}" data-attraction="${area.mainAttraction}" data-id="${area.id}">
+    This is area name is ${area.mainAttraction}
+</li>`;
+    }
+    areaHtml += "</ol>"
+    return areaHtml;
+
 };
+
+const guestHelperCount = (indArea) => {
+    let areaID = parseInt(indArea.dataset.id);
+    let guessCount = 0;
+//    const guest = guests.find(guest => guest.areaId === areaID);
+   for(const guest of guests){
+    if(areaID === parseInt(guest.areaId)){
+        guessCount++;
+    }
+   }
+   return guessCount;
+}
+// another way to do it below 
+// This function calculates the guest count for a specific area
+// const guestHelperCount = (areaID) => {
+//     return guests.filter(guest => guest.areaId === parseInt(areaID)).length;
+// };
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const areaTarget = clickEvent.target
+        let areaName = areaTarget.dataset.attraction;
+        if(areaTarget.dataset.type === "area"){
+            let guessCount = guestHelperCount(areaTarget);
+            window.alert(`there are ${guessCount} guests in this area of ${areaName}`);
+        }
+    }
+)
--- scripts/guests.js
+++ scripts/guests.js
@@ -3,10 +3,10 @@ import { getGuests} from "./database.js"
 const guests = getGuests()
 
 export const Guests = () => {
-    return `
-        <ul>
-            <li></li>
-            <li></li>
-        </ul>
-    `;
+    let guestsHTML = `<ul>`;
+    for (const guest of guests) {
+        guestsHTML += `<li>${guest.name}</li>`;
+    }
+    guestsHTML += `</ul>`;
+    return guestsHTML;
 };
